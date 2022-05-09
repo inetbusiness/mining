b8f9a1540930e7a2611e56995174524288ea09ff
inetbusiness <scarabeuz@gmail.com>
2022-05-09 14:24:19 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  ngOnInit = function () {
+    document.body.className = 'cyber';
+  };
+  ngOnDestroy() {
+    document.body.className = '';
+  }
 }
